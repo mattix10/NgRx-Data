903bb38c89d86ae118e3a67df65ab431943f5b17
mattix10 <mateusz.salamon10@gmail.com>
2022-05-11 20:51:57 +0200

    configuring router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/post.module').then((m) => m.PostsModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
