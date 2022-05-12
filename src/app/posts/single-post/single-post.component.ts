2fe299a6dae038d572d9e66a26ada3e902c0fb33
mattix10 <mateusz.salamon10@gmail.com>
2022-05-12 23:41:01 +0200

    add singlePost component

--- src/app/posts/single-post/single-post.component.ts
+++ src/app/posts/single-post/single-post.component.ts
@@ -1,15 +1,24 @@
+import { Post } from './../../models/post.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../posts.service';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+  // styleUrls: ['./single-post.component.css'],
 })
 export class SinglePostComponent implements OnInit {
-
-  constructor() { }
+  post: Post;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    this.postService.entities$.subscribe((posts) => {
+      this.post = posts.find((post) => post.id === id);
+    });
   }
-
 }
